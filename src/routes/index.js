1ac4ed826c1988974f8dcf918d4909805fd7935d
EvanYY <evan_yangyang@leadscloud.com>
2020-07-29 20:06:56 +0800

    create empty rep demo

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+
+
+export const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: () => import('')
+  // },
+  // {
+  //   path: '/list',
+  //   name: 'List',
+  //   component: () => import('')
+  // }
+]
